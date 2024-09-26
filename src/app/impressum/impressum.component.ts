3b8677d0bf1470ed389323b149fbf0a0fa6f8bea
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-09-26 20:43:59 +0200

    impressum

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
+ 
 export class ImpressumComponent {
+  router=inject(Router);
 
+  navigateURL(){
+    this.router.navigateByUrl('/dashboard');
+  }
 }
