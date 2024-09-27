55f0ed358520e6f6a721b1dc21dad2067a90cc50
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2024-09-27 19:50:49 +0200

    branch try

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
