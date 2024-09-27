a8e9d7262a9565472c2d10e695d0a7fea228155f
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2024-09-28 00:16:50 +0200

    login-styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  isHovered: boolean = false;
 
+  onMouseOver() {
+    this.isHovered = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+  }
 }
