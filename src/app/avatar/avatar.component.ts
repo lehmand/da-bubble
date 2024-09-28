0e04eba79ab2e9a4d14a38f2060ec60996c186a3
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-09-28 17:12:14 +0200

    added changing

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,CommonModule],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
-
+avatarBox:string[]=[
+  '../../assets/img/avatar/avatar1.png',
+  '../../assets/img/avatar/avatar2.png',
+  '../../assets/img/avatar/avatar3.png',
+  '../../assets/img/avatar/avatar4.png',
+  '../../assets/img/avatar/avatar5.png',
+  '../../assets/img/avatar/avatar6.png',
+]
 }
