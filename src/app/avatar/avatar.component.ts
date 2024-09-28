8b169f4c3f078eea0934659253a68628fbdb5264
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-09-28 20:24:33 +0200

    added change

--- src/app/avatar/avatar.component.ts
+++ src/app/avatar/avatar.component.ts
@@ -10,12 +10,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
+
 avatarBox:string[]=[
   '../../assets/img/avatar/avatar1.png',
   '../../assets/img/avatar/avatar2.png',
   '../../assets/img/avatar/avatar3.png',
   '../../assets/img/avatar/avatar4.png',
   '../../assets/img/avatar/avatar5.png',
-  '../../assets/img/avatar/avatar6.png',
-]
+  '../../assets/img/avatar/avatar6.png', 
+];
 }
+
+
