505855a39b0910df319f0fdea0a0e95832d91506
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2024-09-30 21:06:44 +0200

    form valid createUser

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { newUser } from '../interfaces/user.class';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule
+} from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { addingUserService } from '../services/addingUser.service';
 import { LoginComponent } from '../login/login.component';
@@ -9,47 +15,60 @@ import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, LoginComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss',
 })
-export class CreateAccountComponent {
-   userForm: FormGroup;
+export class CreateAccountComponent implements OnInit {
+  createUserForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-     private addingUserService: addingUserService, 
-
+    private addingUserService: addingUserService,
+    private authService: AuthService
   ) {
-    this.userForm = this.formBuilder.group({
+    this.createUserForm = this.formBuilder.group({
       fullName: [''], //für Validation//
-      userMail: [''],
-      password: [''],
+      userMail: '',
+      password: '',
+      checkbox: Boolean
     });
-  } 
+  }
+
+  ngOnInit() {
+    this.createUserForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    checkbox: ['', Validators.required] 
+    });
+  }
+  get f() {
+    return this.createUserForm.controls;
+  }
 
   isDefault: boolean = true;
   isHovered: boolean = false;
   isClicked: boolean = false;
   isChecked: boolean = false;
 
-   async onSubmit() {
+  async onSubmit() {
     const newUser: newUser = {
-      displayName: this.userForm.value.fullName || '',
-      userMail: this.userForm.value.userMail,
-      password: this.userForm.value.password,
+      displayName: this.createUserForm.value.fullName || '',
+      userMail: this.createUserForm.value.userMail,
+      password: this.createUserForm.value.password,
     };
 
     console.log('Neuer Benutzer:', newUser);
     try {
-    //  const userCredential = await this.authService.registerUser(newUser);
+      const userCredential = await this.authService.registerUser(newUser);
       await this.addingUserService.addUser(newUser);
 
       // this.router.navigate(['/avatar-page']); // Route zum Avatar
     } catch (error) {
       console.error('Fehler beim Erstellen des Kontakts:', error);
     }
-  }  
+  }
 
   toggleClicked() {
     this.isClicked = !this.isClicked;
@@ -57,7 +76,6 @@ export class CreateAccountComponent {
 
   toggleChecked() {
     this.isChecked = !this.isChecked;
-    console.log(this.isChecked);
   }
 
   toggleHover() {
