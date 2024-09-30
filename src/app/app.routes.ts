e0ad050bf9cc00ae5945f443c3cbe2a34a59f0ba
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-09-30 15:50:06 +0200

    change resset

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent}
+    {path: '', component: LoginComponent},
+    {path:'reset-password',component:ResetPasswordComponent}
 ];
---
+++ src/app/reset-password/reset-password.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-reset-password',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
+})
+export class ResetPasswordComponent {
+
+}
