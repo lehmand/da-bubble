c25332e47e355e442d4d4e7b45832d5f2d33567a
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-09-30 19:35:32 +0200

    added change

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,CommonModule],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
+
+   sendInfo:boolean=false
+
+  openDiv(){
+    this.sendInfo=!this.sendInfo
+    
+  }
 
 }
