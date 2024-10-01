b2bd3aa1eef703ccb62229147a4248bd31e3e182
Pauline Trotzer <147944434+PaulineTrotzer@users.noreply.github.com>
2024-10-01 23:41:19 +0200

    ngModel loginC

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {
-  FormsModule,
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -19,59 +13,33 @@ import { ResetPasswordComponent } from '../reset-password/reset-password.compone
     MatButtonModule,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
     RouterModule,
-    ResetPasswordComponent
+    ResetPasswordComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
+  loginData = {
+    email: '',
+    password: '',
+  };
   loginFailed = false;
-  errorMessage: string = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService
-  ) {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
-  }
+  ngOnInit() {}
 
-  // Controls
-  get f() {
-    return this.loginForm.controls;
-  }
-
-  async onSubmit() {
-    if (this.loginForm.invalid) {
-      return;
+  async onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid) {
+      console.log('loginData working');
     }
-    const { email, password } = this.loginForm.value;
-
     try {
-      const user = await this.authService.login(email, password);
-      console.log('Login erfolgreich!', user);
-
+      //const user = await this.authService.login(email, password);
       // Weiterleitung auf Dashboard oder andere Seite
     } catch (error: any) {
       console.error('Login fehlgeschlagen:', error);
       this.loginFailed = true;
-
-      // Fehler basierend auf dem Typ setzen
-      if (error.code === 'auth/wrong-password') {
-        this.errorMessage = 'Das Passwort ist falsch.';
-      } else if (error.code === 'auth/user-not-found') {
-        this.errorMessage = 'Es gibt keinen Benutzer mit dieser E-Mail.';
-      } else {
-        this.errorMessage =
-          'Ein unerwarteter Fehler ist aufgetreten. Bitte versuche es erneut.';
-      }
     }
   }
 }
