0a574693857f9f2e10597c1e8e3c43eca93099d0
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-10-06 14:51:49 +0200

    design wip

--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-workspace',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './workspace.component.html',
   styleUrl: './workspace.component.scss'
 })
 export class WorkspaceComponent {
+  constructor() {
+    
+  }
+
+  channelDrawerOpen: boolean = true;
+  messageDrawerOpen: boolean = true;
+
+  toggleChannelDrawer(){
+    this.channelDrawerOpen = !this.channelDrawerOpen;
+    console.log(this.channelDrawerOpen)
+  }
+
+  toggleMessageDrawer(){
+    this.messageDrawerOpen = !this.messageDrawerOpen;
+    console.log(this.messageDrawerOpen)
+  }
 
 }
