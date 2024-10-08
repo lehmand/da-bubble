ef66f2b056d0f5026850250202f0c40f3ce488c1
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-10-08 20:31:50 +0200

    commit

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-start-screen',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,HeaderComponent],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
