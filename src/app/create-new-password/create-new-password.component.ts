6dad68c6eb4fac0744c97a761bc16e623b59c67c
Mesropo <142653068+Mesropo@users.noreply.github.com>
2024-10-13 19:25:14 +0200

    reset password

--- src/app/create-new-password/create-new-password.component.ts
+++ src/app/create-new-password/create-new-password.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { getAuth, confirmPasswordReset } from "firebase/auth";
+import { Firestore,doc,updateDoc} from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-create-new-password',
@@ -12,7 +17,66 @@ import { CommonModule } from '@angular/common';
   styleUrl: './create-new-password.component.scss'
 })
 export class CreateNewPasswordComponent {
-
   sendInfo: boolean = false;
+  disabled: boolean = true;
+  route = inject(ActivatedRoute);
+  checkPasswordinfo: boolean = false;
+  password: string = '';
+  confirmPassword: string = '';
+  oobCode: any;
+  firestore = inject(Firestore);
+  router=inject(Router);
+
+  openDiv() {
+    this.sendInfo = true;
+    setTimeout(() => {
+      this.sendInfo=false;
+    }, 2000);
+  } 
+
+    resetFields():void{
+      this.confirmPassword='';
+      this.password=''
+    }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.oobCode = params['oobCode'];
+      
+    });
+  }
+
+  checkPassworFields() {
+    if (this.password && this.confirmPassword && this.password === this.confirmPassword) {
+      this.disabled = false;
+      this.checkPasswordinfo = false;
+    } else {
+      this.checkPasswordinfo = true;
+      this.disabled = true;
+    }
+  }
+
+  async createNewPassword() {
+    const auth = getAuth();
+    if (!this.oobCode) {
+      return;
+    }
+    await confirmPasswordReset(auth, this.oobCode, this.password);
+    const user = auth.currentUser;
+    if(user){
+      const userUID = user.uid;
+    }
+    this.sendInfo=true;
+    this.resetFields();
+    this.openDiv();
+    this.router.navigate(['/']);
+  } 
 
 }
+// async  updatePasswort(userUID:any){
+//     const userRef = doc(this.firestore, 'users', userUID);
+//     await updateDoc(userRef, { 
+//       // Speichere den Zeitstempel, wann das Passwort geändert wurde
+//       passwordUpdatedAt: new Date() 
+//     });
+//   }
--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
 import { ImpressumComponent } from '../impressum/impressum.component';
+import { Firestore, updateDoc, doc, getDoc,getDocs,collection,query,where} from '@angular/fire/firestore';
+import { ActivatedRoute,Router } from '@angular/router';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 @Component({
   selector: 'app-reset-password',
@@ -20,13 +23,53 @@ import { ImpressumComponent } from '../impressum/impressum.component';
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss',
 })
-export class ResetPasswordComponent {
-  sendInfo: boolean = false;
-
+export class ResetPasswordComponent implements OnInit {
+   sendInfo: boolean = false;
+   disabled:boolean=true;
+   userEmail='';
+   firestore=inject(Firestore)
+   falseEmailInfo:boolean=false;
+   emailValid: boolean = true;
+   router=inject(Router);
+    
   openDiv() {
     this.sendInfo = true;
-    // setTimeout(() => {
-    //   this.sendInfo=false;
-    // }, 2000);
+    setTimeout(() => {
+      this.sendInfo=false;
+    }, 2000);
+  }
+  
+  ngOnInit(): void {
+     
+  }
+  
+  onInputChange(value: string): void {
+    this.userEmail = value; 
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    this.emailValid = emailRegex.test(this.userEmail);
+    this.disabled = !this.emailValid;
+  }
+  
+  async sendEmail() {
+    const docref = collection(this.firestore, 'users');
+    const q = query(docref, where('email', '==', this.userEmail));
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      await this.sendEmailInfo(this.userEmail);
+      this.sendInfo = true;
+      this.userEmail = '';
+      this.openDiv();
+    } else { 
+      this.falseEmailInfo = true;
+    }
+  }
+  
+  async sendEmailInfo(email: string) {
+    const auth = getAuth();
+    const actionCodeSettings = {
+      url: 'http://localhost:4200/create-new-password',
+      handleCodeInApp: true, 
+    };
+    await sendPasswordResetEmail(auth, email, actionCodeSettings);
   }
 }
