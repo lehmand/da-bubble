a1535baaffe2671a812f7be9c102d24dff08bc93
Daniel Lehmann <daniel.lehmann188@gmail.com>
2024-10-31 22:13:57 +0100

    wip adding members

--- src/app/dialog-add-member/dialog-add-member.component.ts
+++ src/app/dialog-add-member/dialog-add-member.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-add-member',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dialog-add-member.component.html',
   styleUrl: './dialog-add-member.component.scss'
 })
@@ -13,5 +14,6 @@ export class DialogAddMemberComponent {
   constructor(){}
 
   channel = inject(MAT_DIALOG_DATA);
+  memberToAdd: any;
 
 }
